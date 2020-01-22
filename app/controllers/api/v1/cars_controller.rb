8b165cce7076677dff09fa5785a0afffceee27a7
Roberson e Marina <roberson.miguel@biosbug.com.br>
2020-01-22 20:23:23 -0300

    WIP API Show usando rescue_from
    Co-authored-by: Mariana <marinanycastro@gmail.com>

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,10 +1,8 @@
 class Api::V1::CarsController < Api::V1::ApiController
   def show
-    if @car = Car.find_by(params[:id])
+    @car = Car.find(params[:id])
     render json: @car
-    else 
-    head 404
-    end
+  
   end
 
   def index
